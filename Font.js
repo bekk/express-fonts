422f3214fa9cf693b64553d2171f2f7f19691379
Moquan Chen <mq.chen@gmail.com>
2017-02-09 09:34:11 +0100

    Added extension to format map

--- Font.js
+++ Font.js
@@ -1,5 +1,14 @@
 const path = require('path');
 
+const EXT_TO_FORMAT = {
+  'svg': 'svg',
+  'otf': 'opentype',
+  'ttf': 'truetype',
+  'woff2': 'woff2',
+  'woff': 'woff',
+  'eot': 'embedded-opentype'
+}
+
 class Font {
   constructor(file) {
     this.file = file;
@@ -7,12 +16,23 @@ class Font {
 
   getMetaData() {
     let obj = {};
+
+    // Format
     obj.format = path.extname(this.file).substr(1);
+    if(EXT_TO_FORMAT.hasOwnProperty(obj.format)) obj.format = EXT_TO_FORMAT[obj.format];
+
+    // Family name
     obj.family = path.dirname(this.file).match(/([^\/]*)\/*$/)[1];
+
+    // Styles
     obj.style = ['italic', 'oblique'].map(v => {
       return path.basename(this.file).toLowerCase().indexOf(v) >= 0 ? v : null;
     }).reduce((a, b) => a || b) || 'normal';
+
+    // Weight
     obj.weight = parseInt((path.basename(this.file).match(/\d+/) || []).slice(-1)[0], 10) || 300;
+
+    // URL
     obj.url = this.file;
 
     return obj;
